87902b357194e457d2f9043b7fa2a282fe6503fb
kucse160235 <m.afnan2016@gmail.com>
2019-09-02 00:20:23 +0600

    Checking inputs and Diagonal Condition Applied

--- main.py
+++ main.py
@@ -30,25 +30,137 @@ while 1:
             print(gridMatrix[i][j], end=" ")
         print("")
     if loop % 2 == 0:
-        print("***Player 1 Turn***")
-        p = int(input("Please select the position for insertion : "))
-        row_index = positions[p][0]
-        col_index = positions[p][1]
-        # print(row_index, col_index, gridMatrix[row_index][col_index])
-        for i in range(3):
-            for j in range(3):
-                if i == row_index and j == col_index:
-                    gridMatrix[i][j] = player_one
-                    break
+        while 1:
+            print("***Player 1 Turn***")
+            p = int(input("Please select the position for insertion : "))
+
+            row_index = positions[p][0]
+            col_index = positions[p][1]
+            # print(row_index, col_index, gridMatrix[row_index][col_index])
+            flag = True
+            for i in range(3):
+                for j in range(3):
+                    if i == row_index and j == col_index:
+                        if gridMatrix[i][j] == '*':
+                            gridMatrix[i][j] = player_one
+                        else:
+                            print("Invalid Input.....Input the correct position.....")
+                            flag = False
+                        break
+            if flag:
+                break
     else:
-        print("***Player 2 Turn***")
-        p = int(input("Please select the position for insertion : "))
-        row_index = positions[p][0]
-        col_index = positions[p][1]
-        # print(row_index, col_index, gridMatrix[row_index][col_index])
+        while 1:
+            print("***Player 2 Turn***")
+            p = int(input("Please select the position for insertion : "))
+            row_index = positions[p][0]
+            col_index = positions[p][1]
+            # print(row_index, col_index, gridMatrix[row_index][col_index])
+            flag = True
+            for i in range(3):
+                for j in range(3):
+                    if i == row_index and j == col_index:
+                        if gridMatrix[i][j] == '*':
+                            gridMatrix[i][j] = player_two
+                        else:
+                            print("Invalid Input.....Input the correct position.....")
+                            flag = False
+                        break
+            if flag:
+                break
+    loop = loop + 1
+
+    # win or lose condition
+    row_win_X = ''
+    row_win_O = ''
+    col_win_X = ''
+    col_win_O = ''
+    diag_win_X = True
+    diag_win_O = True
+
+    k = 2
+    rdiag_win_X = True
+    rdiag_win_O = True
+    for i in range(3):
+        row_win_X = True
+        row_win_O = True
+        col_win_X = True
+        col_win_O = True
+
+        for j in range(3):
+            if gridMatrix[i][j] != 'X':
+                row_win_X = False
+            if gridMatrix[i][j] != 'O':
+                row_win_O = False
+            if gridMatrix[j][i] != 'X':
+                col_win_X = False
+            if gridMatrix[j][i] != 'O':
+                col_win_O = False
+        # condition for diagonal
+        if gridMatrix[i][i] != 'X':
+            diag_win_X = False
+        if gridMatrix[i][i] != 'O':
+            diag_win_O = False
+        # condition for another diagonal
+        if gridMatrix[i][k] != 'X':
+            rdiag_win_X = False
+        if gridMatrix[i][k] != 'O':
+            rdiag_win_O = False
+        k = k-1
+        if row_win_X or row_win_O or col_win_X or col_win_O:
+            break
+
+    if row_win_X or row_win_O or col_win_X or col_win_O or diag_win_X or diag_win_O or rdiag_win_X or rdiag_win_O:
+        print("\n")
         for i in range(3):
             for j in range(3):
-                if i == row_index and j == col_index:
-                    gridMatrix[i][j] = player_two
-                    break
-    loop = loop + 1
+                print(gridMatrix[i][j], end=" ")
+            print("")
+        print("")
+        if row_win_X:
+            if player_one == 'X':
+                print("********  Player 1 Win with X")
+            else:
+                print("********  Player 2 Win with X")
+        elif row_win _O:
+            if player_one == 'O':
+                print("********  Player 1 Win with O")
+            else:
+                print("********  Player 2 Win with O")
+        elif col_win_X:
+            if player_one == 'X':
+                print("********  Player 1 Win with X")
+            else:
+                print("********  Player 2 Win with X")
+        elif col_win_O:
+            if player_one == 'O':
+                print("********  Player 1 Win with O")
+            else:
+                print("********  Player 2 Win with O")
+        elif diag_win_X:
+            if player_one == 'X':
+                print("********  Player 1 Win with X")
+            else:
+                print("********  Player 2 Win with X")
+        elif diag_win_O:
+            if player_one == 'O':
+                print("********  Player 1 Win with O")
+            else:
+                print("********  Player 2 Win with O")
+        elif rdiag_win_X:
+            if player_one == 'X':
+                print("********  Player 1 Win with X")
+            else:
+                print("********  Player 2 Win with X")
+        elif rdiag_win_O:
+            if player_one == 'O':
+                print("********  Player 1 Win with O")
+            else:
+                print("********  Player 2 Win with O")
+        break
+
+    # draw condition
+    if loop > 8:
+        print("\n ***** Match Tied *****")
+        break
+    print("\n")
